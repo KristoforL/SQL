486609b4b72645a3d1664083cbf67c74d09467ec
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-13 18:41:15 -0500

    Update mysql.session.sql
    Learned how to  make comments

--- mysql.session.sql
+++ mysql.session.sql
@@ -28,8 +28,30 @@ SELECT categoryid, COUNT(*) AS 'Total Items' FROM items GROUP BY categoryid;
 SELECT class, COUNT(*) FROM students GROUP BY class HAVING count(*) <20;
 
 SELECT jobtitle, salary, COUNT(*) FROM employees GROUP BY salary;
+/*
 - This returned this for some reason. I have only tested this in mysql
 ERROR 1055 (42000): Expression #1 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'company.employees.jobtitle' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by
 
 but the below sql query worked without jobtitle
+*/
 SELECT salary, COUNT(*) FROM employees GROUP BY salary;
+
+SELECT * FROM students ORDER BY firstname, lastname;
+
+/*
+Will be useful on the assessment
+*/
+SELECT * FROM employees LIMIT 10;
+
+SELECT * FROM students WHERE firstname LIKE 'Jo_';
+SELECT * FROM students WHERE firstname LIKE 'Jo__';
+SELECT * FROM students WHERE firstname LIKE '%_y';
+
+/*
+Helpful for joining
+*/
+
+SELECT s.firstname, s.lastname FROM students AS s;
+
+SELECT CONCAT(firstname, ' ',lastname) AS 'Fullname' FROM students;
+
