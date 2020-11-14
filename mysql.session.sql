8ede52b7aa4125a2363a0566aad4491647f82716
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-14 02:29:54 -0500

    Update mysql.session.sql
    Learned something from stackoverflow

--- mysql.session.sql
+++ mysql.session.sql
@@ -9,9 +9,9 @@ SELECT * FROM students WHERE (age > 6 || firstname <> 'John');
 
 SELECT * FROM students WHERE age IN(5, 6);
 
-SELECT * FROM employees WHERE EXIST (SELECT * FROM projects WHERE projects.employeeid = employees.employeeid);
+SELECT * FROM employees WHERE EXISTS (SELECT * FROM projects WHERE projects.employeeid = employees.employeeid);
 
-SELECT * FROM employees WHERE EXIST (SELECT * FROM projects WHERE projects.employeeid = employees.employeeid AND projectid = 3);
+SELECT * FROM employees WHERE EXISTS (SELECT * FROM projects WHERE projects.employeeid = employees.employeeid AND projectid = 3);
 
 SELECT * FROM students WHERE age BETWEEN 6 AND 10
 
@@ -33,6 +33,11 @@ SELECT jobtitle, salary, COUNT(*) FROM employees GROUP BY salary;
 ERROR 1055 (42000): Expression #1 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'company.employees.jobtitle' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by
 
 but the below sql query worked without jobtitle
+
+Answer from stackovwerflow says i need to have both in the groupby clause as well so it should be
+
+SELECT jobtitle, salary, COUNT(*) FROM employees GROUP BY salary, jobtitle;
+
 */
 SELECT salary, COUNT(*) FROM employees GROUP BY salary;
 
@@ -54,4 +59,12 @@ Helpful for joining
 SELECT s.firstname, s.lastname FROM students AS s;
 
 SELECT CONCAT(firstname, ' ',lastname) AS 'Fullname' FROM students;
+
+insert into students2 select * students;
+
+insert into students2 (firstname, lastname) select firstname, lastname from students;
+
+delete from students2 where studentid = 60;
+
+delete from students2;
 
